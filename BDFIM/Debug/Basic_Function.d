0ca3ca81413532d0531f28c2fdd00361b7c29211
lxxx12 <895159308@qq.com>
2017-11-15 20:15:56 +0800

    Initial checkin of BDFIM

---
+++ BDFIM/Debug/Basic_Function.d
@@ -0,0 +1,65 @@
+# FIXED
+
+Basic_Function.obj: ../Basic_Function.c
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_Device.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_Adc.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_DevEmu.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_CpuTimers.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_ECan.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_ECap.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_DMA.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_EPwm.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_EQep.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_Gpio.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_I2c.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_McBSP.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_PieCtrl.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_PieVect.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_Spi.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_Sci.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_SysCtrl.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_XIntrupt.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_Xintf.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/DSP2833x_Examples.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/DSP2833x_GlobalPrototypes.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/DSP2833x_ePwm_defines.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/DSP2833x_Dma_defines.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/DSP2833x_I2C_defines.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/DSP2833x_DefaultISR.h
+Basic_Function.obj: C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/C28x_FPU_FastRTS.h
+Basic_Function.obj: D:/ccsv7/ccsv7/tools/compiler/ti-cgt-c2000_16.9.4.LTS/include/math.h
+Basic_Function.obj: D:/ccsv7/ccsv7/tools/compiler/ti-cgt-c2000_16.9.4.LTS/include/linkage.h
+Basic_Function.obj: D:/ccsv7/ccsv7/tools/compiler/ti-cgt-c2000_16.9.4.LTS/include/_defs.h
+Basic_Function.obj: ../Basic_Function.h
+
+../Basic_Function.c: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_Device.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_Adc.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_DevEmu.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_CpuTimers.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_ECan.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_ECap.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_DMA.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_EPwm.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_EQep.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_Gpio.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_I2c.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_McBSP.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_PieCtrl.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_PieVect.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_Spi.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_Sci.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_SysCtrl.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_XIntrupt.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_headers/include/DSP2833x_Xintf.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/DSP2833x_Examples.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/DSP2833x_GlobalPrototypes.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/DSP2833x_ePwm_defines.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/DSP2833x_Dma_defines.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/DSP2833x_I2C_defines.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/DSP2833x_DefaultISR.h: 
+C:/Users/lixuan/Desktop/1106/dc3lmc_balance_v1\ -gai/dc3lmc_balance_v1\ -gai/DSP2833x_common/include/C28x_FPU_FastRTS.h: 
+D:/ccsv7/ccsv7/tools/compiler/ti-cgt-c2000_16.9.4.LTS/include/math.h: 
+D:/ccsv7/ccsv7/tools/compiler/ti-cgt-c2000_16.9.4.LTS/include/linkage.h: 
+D:/ccsv7/ccsv7/tools/compiler/ti-cgt-c2000_16.9.4.LTS/include/_defs.h: 
+../Basic_Function.h: 
